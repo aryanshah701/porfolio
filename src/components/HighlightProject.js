0a3e8f72024f24f5e587b7a2b9bcf38cc02fc411
Aryan Shah <aryanshah791@gmail.com>
2021-05-21 13:35:27 -0400

    Change circle spotlights to rectangle

---
+++ src/components/HighlightProject.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import gostudyfeed from '../assets/images/gostudy-feed.png';
+import gostudylanding from '../assets/images/gostudy-landing.png';
+
+const HighlightProject = ({ name, description, imageSrc, idx }) => {
+  let image = null;
+  switch (imageSrc) {
+    case 'gostudyfeed':
+      image = gostudyfeed;
+      break;
+    case 'gostudylanding':
+      image = gostudylanding;
+      break;
+    default:
+      break;
+  }
+
+  return (
+    <section id={idx} className={`wrapper alt spotlight style${idx}`}>
+      <div className="inner">
+        <a href="/#" className="image">
+          <img src={image} alt="Project Image" />
+        </a>
+        <div className="content">
+          <h2 className="major">{name}</h2>
+          <p>{description}</p>
+          <a href="/#" className="special">
+            Learn more
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HighlightProject;
